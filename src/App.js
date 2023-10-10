a40b37bcb0af1e4c16d043c3ff6275cd3af904bc
sivasaikumar <sivasaikumar.pinjala@gmail.com>
2023-10-10 23:39:10 +0530

    layout for profile page was created

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
      
      <Routes>
      <Route path='/' element={<Home/>}/>
-     <Route path='/test' element={<ProfilePage/>}/>
+     <Route path='/test' element={<ProfilePage />}/>
      </Routes>
     </div>
   );
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,7 +1,22 @@
 const ProfilePage = () => {
   return (
-    <div>
-      <h2 className="text-red-700">profile page</h2>
+    <div className="flex justify-around">
+      <div className="border-2 border-indigo-500/100 w-[20%] h-[80vh] mt-[5%] rounded-3xl"></div>
+      <div className="border-2 border-red-500/100 w-[70%] h-[80vh] mt-[5%] rounded-3xl">
+        <div className=" m-7 h-[10vh] w-[90%] rounded-3xl"></div>
+        <div
+          style={{
+            border: "1px solid lightgrey",
+            width: "90%",
+            margin: "auto",
+          }}
+        ></div>
+        <div className="flex justify-around">
+          <div className="border-0 border-none-500/100 h-[55vh] w-[40%] rounded-3xl"></div>
+
+          <div className=" h-[55vh] w-[70%] border-l-2 border-l-lightgrey-500 mt-5"></div>
+        </div>
+      </div>
     </div>
   );
 };
