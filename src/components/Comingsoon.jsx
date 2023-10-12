aa217778883556e5fe1a1b6b9182c482ff128514
sivasaikumar <sivasaikumar.pinjala@gmail.com>
2023-10-12 11:50:31 +0530

    comingsoon page added

---
+++ src/components/Comingsoon.jsx
@@ -0,0 +1,11 @@
+const ComingSoon = ({ index }) => {
+  return (
+    <>
+      <img
+        src="https://img.freepik.com/free-vector/coming-soon-background-with-focus-light-effect-design_1017-27277.jpg?w=740&t=st=1697021501~exp=1697022101~hmac=9728ed32e1f40d973e953be62a46180d8a7a21033bd3f2ff0ec93e58df08a325"
+        alt="coming soon"
+      />
+    </>
+  );
+};
+export default ComingSoon;
